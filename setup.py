4dcf2505be57c65f5a51cc8225c423cfb8059ab3
TanteMatilda <anne.leipertz@googlemail.com>
2023-11-13 20:29:10 +0100

    two new files with the names requirements.txt and setup.py will be uploaded

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='DSSS_Homework_2',
+    version='1.0',
+    author="AnneL",
+    author_email="anne.leipertz@fau.de",
+    packages=find_packages(),
+    install_requires=["numpy"]
+    url= "https://github.com/TanteMatilda/dsss_homework_2.git"
+)
