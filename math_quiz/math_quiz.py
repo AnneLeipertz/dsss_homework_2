3b9084314009c6b183604a9993f5b4abc79e2b25
TanteMatilda <anne.leipertz@googlemail.com>
2023-11-13 17:29:21 +0100

    add the updated version of math_quiz
    import random
    
    def random_int(min, max):
        """
        Generate a random integer within the specified range.
    
        Parameters:
        - min (int): The minimum value of the range (inclusive
        - max (int): The maximum value of the range (inclusive).
    
        Returns:
        int: A random integer within the specified range.
        """
    
        return random.randint(min, max)
    
    def random_operator():
        """
        Choose a random mathematical operator from the set {+, -, *}.
    
        Returns:
        str: A randomly selected mathematical operator.
        """
        return random.choice(['+', '-', '*'])
    
    def math_operation(randInt1, operator, randInt2):
        """
        Perform a mathematical operation on two integers.
    
        Parameters:
        - randInt1 (int): The first operand.
        - operator (str): The mathematical operator.
        - randInt2 (int): The second operand.
    
        Returns:
        tuple: A tuple containing the formatted problem string and the result of the operation.
        """
        # Create a formatted string representing the math problem
        output = f"{randInt1} {operator} {randInt2}"
    
        # Perform the specified operation based on the operator
        if operator == '+': result = randInt1 + randInt2
        elif operator == '-': result = randInt1 - randInt2
        else: result = randInt1 * randInt2
        return output, result
    
    def math_quiz():
        """
        Run a math quiz game.
    
        The game presents math problems to the user and checks their answers and
        at the end prints the user's score of the game.
    
        Side Effects:
            Prints prompts to the console.
    
        Raises:
            ValueError: If the user does not enter a valid integer.
    
        Examples:
            >>> math_quiz()
            Welcome to the Math Quiz Game!
            You will be presented with math problems, and you need to provide the correct answers.
    
            Question: 3 + 4
            Your answer: 7
            Correct! You earned a point.
    
            ...
    
            Game over! Your score is: 8/10
        """
        quizScore = 0
        taskNumbers = 10
    
        # Display initial welcome messages
        print("Welcome to the Math Quiz Game!")
        print("You will be presented with math problems, and you need to provide the correct answers.")
    
        # Loop through a set number of math problems
        for i in range(taskNumbers):
            # Generate random operands and operator
            randInt1 = random_int(-10, 10); randInt2 = random_int(-10, 10); operator = random_operator()
    
            # Get the math problem and its answer
            PROBLEM, ANSWER = math_operation(randInt1, operator,randInt2)
            print(f"\nQuestion: {PROBLEM}")
    
            # Allow the user to attempt answering the question (up to 5 attempts)
            attempts = 0
            while attempts < 5:
                useranswer = input("Your answer: ")
                try:
                    useranswer = int(useranswer)
                    break  # Break the loop if the conversion to int is successful
                except ValueError:
                    print("Your given answer must be an integer. Please try again.")
                attempts += 1
    
            # If the user reaches the maximum attempts, end the quiz
            if attempts ==5:
                print("Too many invalid attempts. The math quiz will stop now.")
                return
    
            useranswer = int(useranswer)
    
            # Check if the user's answer is correct and update the score
            if useranswer == ANSWER:
                print("Correct! You earned a point.")
                quizScore += 1
    
            else:
                print(f"Wrong answer. The correct answer is {ANSWER}.")
    
        # Display the final score
        print(f"\nGame over! Your score is: {quizScore}/{taskNumbers}")
    
    if __name__ == "__main__":
        math_quiz()

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,46 +1,117 @@
 import random
 
 
-def function_A(min, max):
+def random_int(min, max):
     """
-    Random integer.
+    Generate a random integer within the specified range.
+
+    Parameters:
+    - min (int): The minimum value of the range (inclusive).
+    - max (int): The maximum value of the range (inclusive).
+
+    Returns:
+    int: A random integer within the specified range.
     """
+
     return random.randint(min, max)
 
 
-def function_B():
+def random_operator():
+    """
+    Choose a random mathematical operator from the set {+, -, *}.
+
+    Returns:
+    str: A randomly selected mathematical operator.
+    """
     return random.choice(['+', '-', '*'])
 
 
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def math_operation(randInt1, operator, randInt2):
+    """
+    Perform a mathematical operation on two integers.
+
+    Parameters:
+    - randInt1 (int): The first operand.
+    - operator (str): The mathematical operator.
+    - randInt2 (int): The second operand.
+
+    Returns:
+    tuple: A tuple containing the formatted problem string and the result of the operation.
+    """
+    output = f"{randInt1} {operator} {randInt2}"
+    
+    if operator == '+': result = randInt1 + randInt2
+    elif operator == '-': result = randInt1 - randInt2
+    else: result = randInt1 * randInt2
+    return output, result
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
+    """
+    Run a math quiz game.
 
+    The game presents math problems to the user and checks their answers and 
+    at the end prints the user's score of the game.
+
+    Side Effects:
+        Prints prompts to the console.
+
+    Raises:
+        ValueError: If the user does not enter a valid integer.
+
+    Examples:
+        >>> math_quiz()
+        Welcome to the Math Quiz Game!
+        You will be presented with math problems, and you need to provide the correct answers.
+        
+        Question: 3 + 4
+        Your answer: 7
+        Correct! You earned a point.
+        
+        ...
+        
+        Game over! Your score is: 8/10
+    """
+    quizScore = 0
+    taskNumbers = 10
+    
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
-
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
-
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+    
+    for i in range(taskNumbers):
+        randInt1 = random_int(-10, 10); randInt2 = random_int(-10, 10); operator = random_operator()
+        
+        PROBLEM, ANSWER = math_operation(randInt1, operator,randInt2)
         print(f"\nQuestion: {PROBLEM}")
-        useranswer = input("Your answer: ")
+        
+        # Allow the user to attempt answering the question (up to 5 attempts)
+        attempts = 0
+        while attempts < 5:
+            useranswer = input("Your answer: ")
+            try:
+                useranswer = int(useranswer)
+                break  # Break the loop if the conversion to int is successful
+            except ValueError:
+                print("Your given answer must be an integer. Please try again.")
+            attempts += 1 
+        
+        # If the user reaches the maximum attempts, end the quiz
+        if attempts ==5:
+            print("Too many invalid attempts. The math quiz will stop now.")
+            return
+        
         useranswer = int(useranswer)
-
+        
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            quizScore += 1
+
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
-
-    print(f"\nGame over! Your score is: {s}/{t_q}")
+    
+    print(f"\nGame over! Your score is: {quizScore}/{taskNumbers}")
 
 if __name__ == "__main__":
     math_quiz()
+
+    
+  
